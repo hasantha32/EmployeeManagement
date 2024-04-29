922edf4526cdf27594bf96baa924de92181fa260
hasantha32 <hasanthamadushan32@gmail.com>
2024-04-29 11:41:19 +0530

    initial commit

---
+++ src/main/java/com/example/EmployeeManagement/controllers/MemberController.java
@@ -0,0 +1,23 @@
+package com.example.EmployeeManagement.controllers;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public MyDataModel saveMember(@RequestBody MemberDTO memberDTO) {
+        return memberService.saveMember(memberDTO);
+    }
+}
